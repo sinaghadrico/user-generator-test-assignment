f457a0b629a6c705e78f33d2bbda0606e5f109eb
sinaghadri <info@sinaghadri.com>
2023-11-22 18:07:46 +0330

    feat - add useUser hook for calc user data and config for funcs

---
+++ src/hooks/useUser.ts
@@ -0,0 +1,53 @@
+import { useState, useEffect, useCallback, useRef } from "react";
+import { baseApiUrl } from "utils/config";
+import UserData from "interfaces/UserData";
+import userMapper from "mapper/userMapper";
+
+const useUser = () => {
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const prevUserDataRef = useRef<UserData | null>(null);
+
+  const fetchRandomUser = useCallback(async () => {
+    try {
+      const response = await fetch(baseApiUrl);
+
+      if (!response || !response.ok) {
+        throw new Error("Error fetching data");
+      }
+
+      const data = await response.json();
+
+      if (!data || !data.results || !data.results[0]) {
+        throw new Error("Error fetching data");
+      }
+      const newUser: UserData = userMapper(data.results[0]);
+
+      prevUserDataRef.current = userData;
+      setUserData(newUser);
+    } catch (error) {
+      console.error("Error fetching random user");
+    }
+  }, [userData]);
+
+  useEffect(() => {
+    fetchRandomUser();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const isFieldChanged = useCallback(
+    (field: keyof UserData) => {
+      return (
+        prevUserDataRef.current &&
+        prevUserDataRef.current[field] !== userData?.[field]
+      );
+    },
+    [userData]
+  );
+
+  return {
+    userData,
+    fetchRandomUser,
+    isFieldChanged,
+  };
+};
+export default useUser;
