40838b8a1f744680ff5e245643ac5aeacb59b343
sinaghadri <info@sinaghadri.com>
2023-11-22 18:04:30 +0330

    feat - add additional config for vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./src/**/*.{js,ts,jsx,tsx}", "./index.html"],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./index.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        background: "url('/bg.png')",
+      },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
